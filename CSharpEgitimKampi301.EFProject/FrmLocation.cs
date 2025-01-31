1db4bbb11293eb28336c926eca955600980d35ac
Kadir Gündüz <159539238+KadirGunduz13@users.noreply.github.com>
2025-01-31 16:34:54 +0300

    Add files via upload

---
+++ CSharpEgitimKampi301.EFProject/FrmLocation.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi301.EFProject
+{
+    public partial class FrmLocation : Form
+    {
+        public FrmLocation()
+        {
+            InitializeComponent();
+        }
+
+        EgitimKampiEfTravelDbEntities db = new EgitimKampiEfTravelDbEntities();
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var values = db.Location.ToList();
+            dataGridView1.DataSource = values;
+
+        }
+
+        private void FrmLocation_Load(object sender, EventArgs e)
+        {
+            var values = db.Guide.Select(x => new
+            {
+                FullName = x.GuideName + " " + x.GuideSurname,
+                x.GuideId
+            }).ToList();
+            cmbGuide.DisplayMember = "FullName";
+            cmbGuide.ValueMember = "GuideId";
+            cmbGuide.DataSource = values;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Location location = new Location();
+            location.LocationCapacity = byte.Parse(nudCapacity.Value.ToString());
+            location.LocationCity = txtCity.Text;
+            location.LocationCountry = txtCountry.Text;
+            location.LocationPrice = decimal.Parse(txtPrice.Text);
+            location.DayNight = txtDayNight.Text;
+            location.GuideId = int.Parse(cmbGuide.SelectedValue.ToString());
+            db.Location.Add(location);
+            db.SaveChanges();
+            MessageBox.Show("Lokasyon Eklendi");
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var deleteValue = db.Location.Find(id);
+            db.Location.Remove(deleteValue);
+            db.SaveChanges();
+            MessageBox.Show("Lokasyon Silindi");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var updateValue = db.Location.Find(id);
+            updateValue.DayNight = txtDayNight.Text;
+            updateValue.LocationPrice = decimal.Parse(txtPrice.Text);
+            updateValue.LocationCapacity = byte.Parse(nudCapacity.Value.ToString());
+            updateValue.LocationCity = txtCity.Text;
+            updateValue.LocationCountry = txtCountry.Text;
+            updateValue.GuideId = int.Parse(cmbGuide.SelectedValue.ToString());
+            db.SaveChanges();
+            MessageBox.Show("Lokasyon Güncellendi");
+
+        }
+    }
+}
